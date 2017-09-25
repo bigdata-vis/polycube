ca52864aeebdae099ac3a39fb9cf4d5d3e85a16f
Sam <s.salisu@live.co.uk>
2017-09-25 13:12:01 +0100

    new branch integration

--- js/pcmodule_CD.js
+++ js/pcmodule_CD.js
@@ -617,7 +617,6 @@
                 .ticks(dataSlices);
 
             var dateARR = d3.scaleTime().domain([new Date(startDate), new Date(endDate)]);
-
             // console.log(dateARR);
 
             // var separator = height / dateArray.length;
