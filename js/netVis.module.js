f4299483caac360bb8202754aa36f9d8f65526e9
Roger Almeida Leite <rogeraleite@gmail.com>
2019-01-11 18:38:40 +0100

    + Adding labels function

--- js/netVis.module.js
+++ js/netVis.module.js
@@ -9,6 +9,8 @@ let _objects_map = [];
 let _data;
 let _data_map = new Map();
 
+let _textlabels = [];
+
 
 let _date_range = 0; //year(s)
 let _amount_of_layers = 100;
@@ -161,9 +163,16 @@ function init_countries() {
     generateCountriesMetaInfo();
     _data = _data.sort(sortArrayByAge);
 
-    //distributeCountriesCubesInTube();
-    distributeCountriesCubesInForcedLayout();
+    //////////////////////////////////////////
+    // FIRST CALL
+    //////////////////////////////////////////
+    distributeCountriesCubesInTube();
+    //distributeCountriesCubesInForcedLayout();
     distributeCountriesEdges();
+    distributeTextLabels();
+
+   
+    //////////////////////////////////////////
 
     _raycaster = new THREE.Raycaster();
     _mouse = new THREE.Vector2();
@@ -177,11 +186,110 @@ function init_countries() {
     window.addEventListener( 'resize', onWindowResize, false );
 }
 
+function distributeTextLabels(){
+
+    // var text = this._createTextLabel(new THREE.Vector3(0,0,0));
+    // text.setHTML("Labadssadsadsadasdadasdadel");
+    // _textlabels.push(text);
+    // _container.appendChild(text.element);
+
+    _objects.forEach((e,i) => { 
+        console.log(e.info);
+        let size = calc_size(e.info.overall_end, e.info.overall_start);
+
+        var spritey = makeTextSprite( e.info.country, 
+		{ fontsize: size, fontface: "Georgia", borderColor: {r:0, g:0, b:0, a:1.0} } );
+	    spritey.position.set(e.position.x, e.position.y, e.position.z);
+	    _scene.add( spritey );
+    });//end for
+}
+
+
+function calc_size(end, start){
+    let size = end-start;
+    if(size<25) size = 25;
+    else if (size>40) size = 40;
+    return size;
+}
+
+function makeTextSprite( message, parameters )
+  {
+      if ( parameters === undefined ) parameters = {};
+      
+      var fontface = parameters.hasOwnProperty("fontface") ? 
+          parameters["fontface"] : "Arial";
+      
+      var fontsize = parameters.hasOwnProperty("fontsize") ? 
+          parameters["fontsize"] : 18;
+      
+      var borderThickness = parameters.hasOwnProperty("borderThickness") ? 
+          parameters["borderThickness"] : 4;
+      
+      var borderColor = parameters.hasOwnProperty("borderColor") ?
+          parameters["borderColor"] : { r:0, g:0, b:0, a:1.0 };
+      
+      var backgroundColor = parameters.hasOwnProperty("backgroundColor") ?
+          parameters["backgroundColor"] : { r:255, g:255, b:255, a:1.0 };
+  
+      //var spriteAlignment = THREE.SpriteAlignment.topLeft;
+          
+      var canvas = document.createElement('canvas');
+      var context = canvas.getContext('2d');
+      context.font = "Bold " + fontsize + "px " + fontface;
+      
+      // get size data (height depends only on font size)
+      var metrics = context.measureText( message );
+      var textWidth = metrics.width;
+      
+      // background color
+      context.fillStyle   = "rgba(" + backgroundColor.r + "," + backgroundColor.g + ","
+                                    + backgroundColor.b + "," + backgroundColor.a + ")";
+      // border color
+      context.strokeStyle = "rgba(" + borderColor.r + "," + borderColor.g + ","
+                                    + borderColor.b + "," + borderColor.a + ")";
+  
+      context.lineWidth = borderThickness;
+      roundRect(context, borderThickness/2, borderThickness/2, textWidth + borderThickness, fontsize * 1.4 + borderThickness, 6);
+      // 1.4 is extra height factor for text below baseline: g,j,p,q.
+      
+      // text color
+      context.fillStyle = "rgba(0, 0, 0, 1.0)";
+  
+      context.fillText( message, borderThickness, fontsize + borderThickness);
+      
+      // canvas contents will be used for a texture
+      var texture = new THREE.Texture(canvas) 
+      texture.needsUpdate = true;
+  
+      var spriteMaterial = new THREE.SpriteMaterial( 
+          { map: texture, useScreenCoordinates: false } );
+      var sprite = new THREE.Sprite( spriteMaterial );
+      sprite.scale.set(100,50,1.0);
+      return sprite;	
+  }
+
+// function for drawing rounded rectangles
+function roundRect(ctx, x, y, w, h, r) {
+    ctx.beginPath();
+    ctx.moveTo(x+r, y);
+    ctx.lineTo(x+w-r, y);
+    ctx.quadraticCurveTo(x+w, y, x+w, y+r);
+    ctx.lineTo(x+w, y+h-r);
+    ctx.quadraticCurveTo(x+w, y+h, x+w-r, y+h);
+    ctx.lineTo(x+r, y+h);
+    ctx.quadraticCurveTo(x, y+h, x, y+h-r);
+    ctx.lineTo(x, y+r);
+    ctx.quadraticCurveTo(x, y, x+r, y);
+    ctx.closePath();
+    ctx.fill();
+	ctx.stroke();   
+}
+
 function distributeCountriesEdges(){
 
     _data.forEach((e,i) => { 
         e.events_id_list.forEach(id=>{
-                createCountriesLinkById(id);
+            createCountriesLinkById(id);
         });
     });//end for
 
@@ -378,10 +486,13 @@ function distributeCountriesCubesInTube(){
         _data_map.get(e.country).position = object.position;
 
         _scene.add(object);
-        _objects.push(object);
+        _objects.push(object);      
+
     });//end for
 
 }
+
+
 
 function distributeCountriesCubesInForcedLayout(){
     let cube_size = 5;
